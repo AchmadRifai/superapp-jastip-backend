fbff09fa7695bd35151f04daad5db67107f25efb
AchmadRifai <officialrdevel@gmail.com>
2021-10-13 00:24:58 +0700

    Init commit

---
+++ src/main/java/achmad/rifai/jastip/SuperappJastipBackendApplication.java
@@ -0,0 +1,13 @@
+package achmad.rifai.jastip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappJastipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperappJastipBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/achmad/rifai/jastip/SuperappJastipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package achmad.rifai.jastip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperappJastipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
